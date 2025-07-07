3c70022fd92747cde0ca8b06d3578a744bbc0e81
Thomas Schmahl <thmschmahl@gmail.com>
2025-07-07 13:58:31 +0200

    fix: type, lint and missing keys

--- src/components/Coffe.tsx
+++ src/components/Coffe.tsx
@@ -4,11 +4,10 @@ import Image from "next/image";
 
 
 export function Coffee({ recipe }: { recipe: Recipe }) {
-  if (recipe === null) { return null }
   const isURL = typeof recipe?.image === 'string' && URL.canParse(recipe.image);
   return <div>
     <h2>{recipe.title}</h2>
     <p>{recipe.description}</p>
-    {isURL && <Image src={recipe.image} alt={recipe.title} width={640} height={480} />}
+    {isURL && <Image src={recipe.image!} alt={recipe.title} width={640} height={480} />}
   </div>
 }
--- src/pages/iced.tsx
+++ src/pages/iced.tsx
@@ -11,13 +11,14 @@ export interface Recipe {
 export const getStaticProps = (async () => {
   try {
     const res = await fetch('http://localhost:3002/api/iced')
-    const recipes: Recipe[] = await res.json()
+    const recipes: (Recipe | null)[] = await res.json()
     return { props: { recipes } }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
     console.error(e.message)
     throw new Error('Fehler bei laden der Daten')
   }
-}) satisfies GetStaticProps<{ recipes: Recipe[] }>
+}) satisfies GetStaticProps<{ recipes: (Recipe | null)[] }>
 
 export default function HotCoffeePage(
   { recipes }: InferGetStaticPropsType<typeof getStaticProps>) {
@@ -25,11 +26,17 @@ export default function HotCoffeePage(
 
   return <div>
     <h1>Iced Coffees</h1>
-    {recipes.map(recipe =>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Coffee recipe={recipe} />
-      </Suspense>
+    {recipes.map(recipe => {
+      if (recipe !== null) {
+        return <Suspense
+          key={recipe.id}
+          fallback={<div>Loading...</div>}>
+          <Coffee recipe={recipe} />
+        </Suspense>
+      }
+    }
     )}
+
   </div>
 }
 
